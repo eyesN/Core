60f2e2f736430b46e1403532cc086f4f872bf213
Aditya Negi <adityanegi7311masterball@gmail.com>
2026-01-07 22:28:32 +0530

    ...

--- T.py
+++ T.py
@@ -1,1 +1,12 @@
-
+import pandas as pd
+import numpy as np
+from PIL import Image
+path ="/Users/adityanegi/Downloads/PFp.jpeg"
+img_array = np.array(Image.open(path))
+s = pd.Series([img_array])
+print(s)
+'''
+ X = [1,2,3,4,5]
+s1 = pd.Series(X)
+print (s1)
+array_s1 = image_series.map()'''
